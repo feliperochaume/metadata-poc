93ca9151a12dfa706dcbfb313ed3705f1efe4984
Edilson R Lima <edilson.lima@meta.com.br>
2023-09-29 11:55:42 -0400

    Ajustes na home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import styles from '@/styles/Home.module.scss';
 import Card from '@/components/Card';
 
-import styles from '@/styles/Home.module.scss';
-
-interface OperatorData {
-  id_operador: number;
-  nome_operador: string;
-  telefone_operador: string;
-  id_application: number;
-  data_application: string;
-  grupo_whatsapp: string;
-  status_application: string;
-}
-
-interface FormattedData {
-  quantidade: number;
-  operator: OperatorData;
-  stage: 'lendario' | 'mestre' | 'estrela';
+interface CadastroData {
+  classificacao: "lendario" | "mestre" | "estrela";
+  quantidade_cadastros: number;
+  operadores: string[];
 }
 
 export default function Home() {
-  const [data, setData] = React.useState<FormattedData[]>([]);
+  const [data, setData] = useState<CadastroData[]>([]);
 
-  React.useEffect(() => {
-    async function fetchCadastros() {
-      const response = await axios.get('/api/cadastros');
-      setData(response.data);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get<CadastroData[]>('/api/cadastros');
+        setData(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
     }
 
-    fetchCadastros();
+    fetchData();
   }, []);
 
   // Função para definir a meta com base no estágio
@@ -44,23 +36,20 @@ export default function Home() {
     } else {
       return 0; // Valor padrão, você pode ajustar conforme necessário
     }
-  }  
+  } 
 
   return (
     <div className={styles.homeContainer}>
-      {data.length > 0 && (
-        <>
-          {data.map((item, index) => (
-            <Card
-              key={index}
-              stage={item.stage}
-              goal={(getGoal(item.stage))} // Defina a meta com base no estágio
-              current={item.quantidade}
-              operatorName={item.operator.nome_operador}
-            />
-          ))}
-        </>
-      )}
+      {data.length > 0 &&
+        data.map((item, index) => (
+          <Card
+            key={index}
+            stage={item.classificacao}
+            goal={(getGoal(item.classificacao))} // Defina a meta com base no estágio
+            current={item.quantidade_cadastros}
+            operatorNames={item.operadores}
+          />
+        ))}
     </div>
-  )
+  );
 }
