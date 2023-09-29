df28c35947f80975769c5935865353d8d5c0019d
Edilson R Lima <edilson.lima@meta.com.br>
2023-09-29 11:55:09 -0400

    refatoração do endpoint /api/cadastros

--- src/pages/api/cadastros.ts
+++ src/pages/api/cadastros.ts
@@ -1,67 +1,95 @@
-import { mockedData } from '@/mocks/data';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { mockedData } from '@/mocks/data';
 
-interface OriginalData {
-  id_operador: number;
+// Interface para representar um cadastro com sua quantidade
+interface CadastroQuantidade {
   nome_operador: string;
-  telefone_operador: string;
-  id_application: number;
-  data_application: string;
-  grupo_whatsapp: string;
-  status_application: string;
+  quantidade_cadastros: number;
 }
 
-// Seus dados originais
-const data: OriginalData[] = mockedData;
+// Enum para as classificações de acordo com a quantidade de cadastros
+enum Classificacao {
+  Estrela = 'estrela',
+  Mestre = 'mestre',
+  Lendario = 'lendario',
+}
 
-// Função para contar duplicatas e gerar operadores únicos com contagem
-function countDuplicatesAndGenerateUniqueOperators(data: OriginalData[]) {
-  const operatorMap: Record<number, { operator: OriginalData, count: number, stage: string }> = {};
+// Função para contar a quantidade de cadastros por operador
+function contarCadastrosPorOperador(): Record<string, number> {
+  const cadastroPorOperador: Record<string, number> = {};
 
-  for (const item of data) {
-    const { id_operador } = item;
-
-    if (operatorMap[id_operador]) {
-      operatorMap[id_operador].count++;
-    } else {
-      operatorMap[id_operador] = { operator: item, count: 1, stage: '' };
-    }
-  }
-
-  const sortedOperators = Object.values(operatorMap)
-    .sort((a, b) => b.count - a.count);
-
-  let currentStage = '';
-
-  return sortedOperators.map(({ operator, count }) => {
-    let stage = currentStage;
-    if (count <= 6) {
-      stage = 'estrela';
-    } else if (count <= 12) {
-      stage = 'mestre';
-    } else {
-      stage = 'lendario';
-    }
-
-    if (stage !== currentStage) {
-      currentStage = stage;
-    }
-
-    return {
-      operator,
-      quantidade: count,
-      stage,
-    };
+  mockedData.forEach((cadastro) => {
+    const nomeOperador = cadastro.nome_operador;
+    cadastroPorOperador[nomeOperador] = (cadastroPorOperador[nomeOperador] || 0) + 1;
   });
+
+  return cadastroPorOperador;
 }
 
+// Função para converter o objeto em um array de objetos
+function converterParaArray(cadastroPorOperador: Record<string, number>): CadastroQuantidade[] {
+  return Object.entries(cadastroPorOperador).map(([nomeOperador, quantidadeCadastros]) => ({
+    nome_operador: nomeOperador,
+    quantidade_cadastros: quantidadeCadastros,
+  }));
+}
+
+// Função para agrupar operadores por quantidade de cadastros
+function agruparPorQuantidade(cadastroPorOperadorArray: CadastroQuantidade[]): Record<number, string[]> {
+  const cadastrosPorQuantidade: Record<number, string[]> = {};
+
+  cadastroPorOperadorArray.forEach((item) => {
+    const quantidade = item.quantidade_cadastros;
+    if (!cadastrosPorQuantidade[quantidade]) {
+      cadastrosPorQuantidade[quantidade] = [];
+    }
+    cadastrosPorQuantidade[quantidade].push(item.nome_operador);
+  });
+
+  return cadastrosPorQuantidade;
+}
+
+// Função para determinar a classificação com base na quantidade de cadastros
+function getClassificacao(quantidadeCadastros: number): Classificacao {
+  if (quantidadeCadastros >= 7 && quantidadeCadastros <= 12) {
+    return Classificacao.Mestre;
+  } else if (quantidadeCadastros >= 13 && quantidadeCadastros <= 32) {
+    return Classificacao.Lendario;
+  }
+  return Classificacao.Estrela;
+}
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    const uniqueOperatorsWithCount = countDuplicatesAndGenerateUniqueOperators(data);
+    // Conta a quantidade de cadastros por operador
+    const cadastroPorOperador = contarCadastrosPorOperador();
 
-    res.status(200).json(uniqueOperatorsWithCount);
+    // Converte o objeto em um array de objetos
+    const cadastroPorOperadorArray = converterParaArray(cadastroPorOperador);
+
+    // Ordena o array em ordem decrescente
+    cadastroPorOperadorArray.sort((a, b) => b.quantidade_cadastros - a.quantidade_cadastros);
+
+    // Agrupa operadores por quantidade de cadastros
+    const cadastrosPorQuantidade = agruparPorQuantidade(cadastroPorOperadorArray);
+
+    // Cria um novo array com classificação
+    const novoArray = Object.keys(cadastrosPorQuantidade).map((quantidade) => {
+      const quantidadeCadastros = parseInt(quantidade, 10);
+      const classificacao = getClassificacao(quantidadeCadastros);
+
+      return {
+        quantidade_cadastros: quantidadeCadastros,
+        operadores: cadastrosPorQuantidade[quantidadeCadastros],
+        classificacao: classificacao,
+      };
+    });
+
+    // Ordena o novo array em ordem decrescente com base na quantidade_cadastros
+    novoArray.sort((a, b) => b.quantidade_cadastros - a.quantidade_cadastros);
+
+    res.status(200).json(novoArray);
   } else {
-    res.status(405).json({ message: 'Método não permitido' });
+    res.status(405).end(); // Método não permitido
   }
 };
