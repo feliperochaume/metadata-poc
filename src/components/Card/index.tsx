465fa5896160d0e65c5ef8947b01cc3c6db3098c
Edilson R Lima <edilson.lima@meta.com.br>
2023-09-29 11:55:32 -0400

    ajustesno  card-component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,10 +7,10 @@ interface Props {
   stage: "lendario" | "mestre" | "estrela";
   goal: number;
   current: number;
-  operatorName: string;
+  operatorNames: string[];
 }
 
-const Card: React.FC<Props> = ({ stage, goal, current, operatorName }) => {
+const Card: React.FC<Props> = ({ stage, goal, current, operatorNames }) => {
   const totalCadastros = goal; // Valor total de cadastros
   const cadastrosConcluidos = current; // Valor atual de cadastros concluídos
   const porcentagemConcluida = (cadastrosConcluidos / totalCadastros) * 100;
@@ -21,13 +21,13 @@ const Card: React.FC<Props> = ({ stage, goal, current, operatorName }) => {
   function formatStringToTitleCase(inputString: string): string {
     // Dividir a string em palavras usando espaço como separador
     const words = inputString.split(' ');
-  
+
     // Mapear cada palavra e formatá-la para o formato de título
     const titleCaseWords = words.map(word => {
       // Converter a primeira letra da palavra para maiúscula e o restante para minúscula
       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     });
-  
+
     // Juntar as palavras formatadas de volta em uma única string
     return titleCaseWords.join(' ');
   }
@@ -42,8 +42,12 @@ const Card: React.FC<Props> = ({ stage, goal, current, operatorName }) => {
       </div>
       <p className={styles.metaText}>{cadastrosConcluidos} de {totalCadastros} cadastros para garantir 1 voucher IFood de R$30!</p>
       <section className={styles.operatorSection}>
-        <Image src={reward_icon} alt="Reward Icon" width={24} height={24} />
-        <h3>{formatStringToTitleCase(operatorName)}</h3>
+        {operatorNames.map((operatorName, index) => (
+          <div key={index} className={styles.operatorInfo}>
+            <Image src={reward_icon} alt="Reward Icon" width={24} height={24} />
+            <h3>{formatStringToTitleCase(operatorName)}</h3>
+          </div>
+        ))}
       </section>
       <button className={styles.cardBtn}>
         Já garantiu R$ 128 nesta rodada!
